c89796fb889b59faef5329e8c23d91da7e07c75b
宝宝 <15601215180@163.com>
2020-07-10 15:21:42 +0800

    缓存

--- src/main/java/com/example/demo_es/DemoEsApplication.java
+++ src/main/java/com/example/demo_es/DemoEsApplication.java
@@ -5,11 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.example.demo_es.dao")
 @SpringBootApplication
 @EnableSwagger2
+@EnableCaching //开启注解
 public class DemoEsApplication {
 
     public static void main(String[] args) {

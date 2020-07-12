32b69bea102a6a43ffbda46549ecdb25a4b59ea3
宝宝 <15601215180@163.com>
2020-07-12 14:36:06 +0800

    jsp

--- src/main/java/com/example/demo_es/service/AddressService.java
+++ src/main/java/com/example/demo_es/service/AddressService.java
@@ -4,6 +4,7 @@ import com.example.demo_es.dao.AddressMapper;
 import com.example.demo_es.repository.AddressRepository;
 import com.example.demo_es.entity.Address;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,12 +20,13 @@ public class AddressService {
     public int insert(Address address){
         int insert = mapper.insert(address);
         Address save = addressdao.save(address);
-        if(save==null&&insert==1){
+        if(save==null&&insert==0){
             throw new RuntimeException("添加失败");
         }else {
             return insert;
         }
     }
+
     public List<Address> selectAll(){
         List<Address> addresses = mapper.selectAll();
         return addresses;
@@ -41,6 +43,24 @@ public class AddressService {
         return byNameLike;
     }
 
-    //
+    public int updateAddress(Address address){
+        int i = mapper.updateAddress(address);
+        Address save = addressdao.save(address);
+        if(save==null&&i!=1){
+            throw new RuntimeException("添加失败");
+        }else {
+            return i;
+        }
+    }
+    public int deleteAddress(int id){
+        int i = mapper.deleteAddress(id);
+        addressdao.deleteById(id);
+        if(i==0){
+            throw new RuntimeException("修改失败");
+        }else {
+            return i;
+        }
+    }
+
 
 }
